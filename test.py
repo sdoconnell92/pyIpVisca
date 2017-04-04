fb4949caf735e63b3f2a65deb3611ddd9d8279b2
Sean O'Connell <sdoconnell92@gmail.com>
2017-04-04 14:34:56 -0700

    Clear now sent before every message

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import SimpleOSCtoUDPConvert as SOUC
 
 ip = "192.168.0.140"
 port = 52381
-message = '010000050000000081010604ff'
+clear = '01000000ffffffff'
+home = '010000050000000081010604ff'
+preset0 = '01000007000000008101043f0201ff'
 
-SOUC.send_udp_packet(ip, port, message)
+SOUC.send_udp_packet(ip, port, home)
